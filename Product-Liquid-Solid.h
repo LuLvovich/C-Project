2994545df3bb6f07c452addf15d308e553997d6f
LuLvovich <im727199@gmail.com>
2026-03-12 11:47:52 +0300

    Product-Liquid-Solid.h FinalVersion

--- Product-Liquid-Solid.h
+++ Product-Liquid-Solid.h
@@ -2,8 +2,8 @@
 // Created by Luka on 11.03.2026.
 //
 
-#ifndef C_PROJECT_INVENTORY_H
-#define C_PROJECT_INVENTORY_H
+#ifndef PRODUCT_LIQUID_SOLID_H
+#define PRODUCT_LIQUID_SOLID_H
 
 #include <string>
 
@@ -16,19 +16,53 @@ protected:
     int price;
     char size;
 public:
-    Product(std::string name, int id, int price, char size);
+    Product()=delete;
     virtual ~Product() = default;
 
     void GetPrice() const;
     int GetId() const;
     std::string GetName() const;
-    char getSize() const;
+    char GetSize() const;
 
-    void ChangePrice(int price);
+    void ChangePrice(int NewPrice);
 
-    void display() const;
+    virtual void display() const;
+    virtual void give() const;
 };
 
-class LiquidProduct: public Product{};
-class SolidProduct: public Product{};
-#endif //C_PROJECT_INVENTORY_H
+class LiquidProduct: public Product{
+protected:
+    int VolumeML;
+    bool IsSparkling;
+
+public:
+    LiquidProduct(const std::string& name, int id, int price, char size, int V, bool Sprkl);
+
+
+    void ChangeVolume(int NewVolume);
+
+    int GetVolumeML() const;
+    bool GetIsSparkling() const;
+
+    virtual void display() const override;
+    virtual void give() const override;
+   };
+
+
+class SolidProduct: public Product{
+protected:
+    int MassGR;
+    bool CarefulDisp;
+public:
+    SolidProduct(const std::string& name, int id, int price, char size, int M, bool CrflDsp);
+
+
+    void ChangeMass(int NewMass);
+
+    int GetMass() const;
+    bool GetCarefulDisp() const;
+
+    virtual void display() const override;
+    virtual void give() const override;
+};
+#endif
